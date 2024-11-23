b36c8394716e9b50858c6e8ed384df9ea12ce8e3
Garry028 <gmisal2002@gmail.com>
2024-11-23 19:02:32 +0530

    feat: added PWA setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withPWA =  withPWAInit({
+  dest: "public",
+  disable:false,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline:true,
+  workboxOptions:{
+    disableDevLogs: false,
+  }
+});
+
+
+
+export default withPWA(nextConfig);
